8b6a23a478b1236be938d631957821a60624c76a
Rucha Mahabal <ruchamahabal2@gmail.com>
2025-07-30 21:05:00 +0530

    feat: add doctypes

---
+++ mini_pos/mini_pos/doctype/invoice_item/invoice_item.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Rucha Mahabal and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class InvoiceItem(Document):
+	pass
